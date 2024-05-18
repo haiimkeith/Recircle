0992a3f57ec151444e80bbd3df2e967f2fcfb23d
haiimkeith <haiimkeith@gmail.com>
2024-05-17 21:03:14 -0700

    Styling Nav Bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Navbar from "./Navbar";
 import "./App.css";
 
 function App() {
-  return <h1 className="poetsen-one-regular">Recircle</h1>;
+  return (
+    <div>
+      <h1 className="poetsen-one-regular">Recircle</h1>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/Navbar.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  const [selectedNavLink, setSelectedNavLink] = useState(null);
+  const [fadeClass, setFadeClass] = useState("");
+
+  const handleNavLinkClick = (link) => {
+    if (link !== selectedNavLink) {
+      setFadeClass("fade-out");
+      setTimeout(() => {
+        setSelectedNavLink(link);
+        setFadeClass("fade-in");
+      }, 300);
+    }
+  };
+
+  // JSON of all available categories
+  const clothingCategories = {
+    Men: ["Tops", "Bottoms", "Outerwear", "Socks", "Underwear", "Balls"],
+    Women: ["Tops", "Bottoms", "Outerwear", "Socks", "Underwear", "Woman"],
+    Kids: ["Tops", "Bottoms", "Outerwear", "Socks", "Underwear", "Kids"],
+    Unisex: ["Tops", "Bottoms", "Outerwear", "Socks", "Underwear", "Uni"],
+  };
+
+  return (
+    <div>
+      <div className="navigation">
+        <a
+          href="#"
+          className={selectedNavLink === "Men" ? "active" : "inactive"}
+          onClick={() => handleNavLinkClick("Men")}
+        >
+          MEN
+        </a>
+        <a
+          href="#"
+          className={selectedNavLink === "Women" ? "active" : "inactive"}
+          onClick={() => handleNavLinkClick("Women")}
+        >
+          WOMEN
+        </a>
+        <a
+          href="#"
+          className={selectedNavLink === "Kids" ? "active" : "inactive"}
+          onClick={() => handleNavLinkClick("Kids")}
+        >
+          KIDS
+        </a>
+        <a
+          href="#"
+          className={selectedNavLink === "Unisex" ? "active" : "inactive"}
+          onClick={() => handleNavLinkClick("Unisex")}
+        >
+          UNISEX
+        </a>
+        <a href="#"></a>
+      </div>
+
+      <div className={`cloth-category-section ${fadeClass}`}>
+        {selectedNavLink && (
+          <ul>
+            {clothingCategories[selectedNavLink].map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
