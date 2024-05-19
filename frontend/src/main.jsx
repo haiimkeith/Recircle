a7466f87e5d2d0fae592266aee9a7d6999f26c0a
haiimkeith <haiimkeith@gmail.com>
2024-05-18 23:19:19 -0700

    Create Item Component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Shop from "./Shop.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    {/* <Shop /> */}
   </React.StrictMode>
-  // This is a branch
 );
